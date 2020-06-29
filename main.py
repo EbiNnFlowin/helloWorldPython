9817ed93f36c490c73f8f3adca2dc6ae45105a34
ebintobin <ebin1234578@gmail.com>
2020-06-29 21:57:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#count = 0
+#for letters in "Hello World":
+ #   if letters == "l":
+  #      count += 1
+#print(count, "letters found")
+test = "Python Programing"
+print("String: ",test)
+first_character = test[:1]
+
+print("First Characters: ", first_character)
+
+last_character = test[-1:]
+print("Last Characters: ",last_character)
